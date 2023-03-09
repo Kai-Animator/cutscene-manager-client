c79fafe441c3416c1ac174f210b409aa836d9843
Kai-Animator <kaireml@protonmail.com>
2023-03-09 14:21:41 +0900

    adjust render path

--- src/components/Cutscenes.tsx
+++ src/components/Cutscenes.tsx
@@ -17,12 +17,12 @@ interface AxiosCs {
 }
 
 interface Props {
-  isSettings: boolean
+  isSettings: boolean;
   setIsSettings: React.Dispatch<React.SetStateAction<boolean>>;
   setRefresh: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-function Cutscenes({isSettings, setIsSettings, setRefresh}: Props) {
+function Cutscenes({ isSettings, setIsSettings, setRefresh }: Props) {
   const [allCsInfo, setAllCsInfo] = useState<any | null>(null);
 
   useEffect(() => {
@@ -30,10 +30,9 @@ function Cutscenes({isSettings, setIsSettings, setRefresh}: Props) {
   }, [isSettings]);
 
   async function getAllCutscenes(): Promise<void> {
-    const allCutscenes: AxiosCs = await axios(
-      'https://java-server-csmanager.onrender.com/cutscenes'
+    await fetch('https://java-server-csmanager.onrender.com/cutscenes').then(
+      (data) => setAllCsInfo(data.body)
     );
-    setAllCsInfo(allCutscenes.data);
   }
 
   function handleNewScene(e: MouseEvent): void {
@@ -66,7 +65,9 @@ function Cutscenes({isSettings, setIsSettings, setRefresh}: Props) {
           </tbody>
         </table>
       </div>
-      {isSettings && <Settings setIsSettings={setIsSettings} setRefresh={setRefresh}/>}
+      {isSettings && (
+        <Settings setIsSettings={setIsSettings} setRefresh={setRefresh} />
+      )}
       <button
         className={`add-cs-button ${tailAddButton}`}
         onClick={handleNewScene}
